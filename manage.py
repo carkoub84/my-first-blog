45b4591e84258fe5f904e9de0dee79a0ce077eaf
Chabha Arkoub <carkoub@ivytech.edu>
2023-12-12 21:25:34 -0500

    Modified templates to display posts from the database.

--- manage.py
+++ manage.py
@@ -20,3 +20,37 @@ def main():
 
 if __name__ == '__main__':
     main()
+Post.objects.all()
+Traceback (most recent call last):
+      File "<console>", line 1, in <module>
+NameError: name 'Post' is not defined
+from blog.models import Post
+Post.objects.all()
+<QuerySet [<Post: my post title>, <Post: another post title>]>
+ost.objects.create(author=me, title='Sample title', text='Test')
+from django.contrib.auth.models import User
+User.objects.all()
+<QuerySet [<User: ola>]>
+me = User.objects.get(username='ola')
+Post.objects.create(author=me, title='Sample title', text='Test')
+<Post: Sample title>
+ost.objects.all()
+<QuerySet [<Post: my post title>, <Post: another post title>, <Post: Sample title>]>
+Post.objects.filter(author=me)
+<QuerySet [<Post: Sample title>, <Post: Post number 2>, <Post: My 3rd post!>, <Post: 4th title of post>]>
+Post.objects.filter(title__contains='title')
+<QuerySet [<Post: Sample title>, <Post: 4th title of post>]>
+from django.utils import timezone
+>>> Post.objects.filter(published_date__lte=timezone.now())
+<QuerySet []>
+post = Post.objects.get(title="Sample title")
+post.publish()
+Post.objects.filter(published_date__lte=timezone.now())
+<QuerySet [<Post: Sample title>]>
+Post.objects.order_by('created_date')
+<QuerySet [<Post: Sample title>, <Post: Post number 2>, <Post: My 3rd post!>, <Post: 4th title of post>]>
+Post.objects.order_by('-created_date')
+<QuerySet [<Post: 4th title of post>,  <Post: My 3rd post!>, <Post: Post number 2>, <Post: Sample title>]>
+Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+<QuerySet [<Post: Post number 2>, <Post: My 3rd post!>, <Post: 4th title of post>, <Post: Sample title>]>
+exit()
