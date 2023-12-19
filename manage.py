ef5e2b44cbc756035749d27ea9bb34a6e32d6a38
Chabha Arkoub <carkoub@ivytech.edu>
2023-12-18 23:28:46 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- manage.py
+++ manage.py
@@ -3,10 +3,9 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,40 +16,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
     main()
-Post.objects.all()
-Traceback (most recent call last):
-      File "<console>", line 1, in <module>
-NameError: name 'Post' is not defined
-from blog.models import Post
-Post.objects.all()
-<QuerySet [<Post: my post title>, <Post: another post title>]>
-ost.objects.create(author=me, title='Sample title', text='Test')
-from django.contrib.auth.models import User
-User.objects.all()
-<QuerySet [<User: ola>]>
-me = User.objects.get(username='ola')
-Post.objects.create(author=me, title='Sample title', text='Test')
-<Post: Sample title>
-ost.objects.all()
-<QuerySet [<Post: my post title>, <Post: another post title>, <Post: Sample title>]>
-Post.objects.filter(author=me)
-<QuerySet [<Post: Sample title>, <Post: Post number 2>, <Post: My 3rd post!>, <Post: 4th title of post>]>
-Post.objects.filter(title__contains='title')
-<QuerySet [<Post: Sample title>, <Post: 4th title of post>]>
-from django.utils import timezone
->>> Post.objects.filter(published_date__lte=timezone.now())
-<QuerySet []>
-post = Post.objects.get(title="Sample title")
-post.publish()
-Post.objects.filter(published_date__lte=timezone.now())
-<QuerySet [<Post: Sample title>]>
-Post.objects.order_by('created_date')
-<QuerySet [<Post: Sample title>, <Post: Post number 2>, <Post: My 3rd post!>, <Post: 4th title of post>]>
-Post.objects.order_by('-created_date')
-<QuerySet [<Post: 4th title of post>,  <Post: My 3rd post!>, <Post: Post number 2>, <Post: Sample title>]>
-Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-<QuerySet [<Post: Post number 2>, <Post: My 3rd post!>, <Post: 4th title of post>, <Post: Sample title>]>
-exit()
+
